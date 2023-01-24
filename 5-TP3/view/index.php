3b39290540b054cc5de1807dadae1751afd736e6
59013-90-16 <59013-90-16@exchange.ad.afpanet>
2023-01-24 15:57:30 +0100

    tp3

---
+++ 5-TP3/view/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="./styleindex.css">
+</head>
+
+<body>
+
+
+
+
+
+    <form enctype="multipart/form-data" action="../controller/executeFonc.php" method="post" >
+
+            <div class="btn">
+                <input class="file , inputbtn " name="file" type="file"> </input>  
+               
+            </div>
+
+            <input class="submit" name="submit" type="submit" value="valider" />
+        
+    </form>
+
+
+
+
+      
+
+</body>
+
+</html>
