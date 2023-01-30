4c41d3f6e26c34486d308e1c02d3373c390cf659
59013-90-16 <59013-90-16@exchange.ad.afpanet>
2023-01-30 16:32:01 +0100

    tp5

---
+++ 7-TP5/Controller/executeInsert.php
@@ -0,0 +1,21 @@
+<?php
+include "../Modele/DBManagement.php";
+
+
+
+
+$nom = $_GET['nom'];
+$prenom = $_GET['prenom'];
+$place = $_GET['place'];
+
+$tabStag = array($nom ,$prenom ,$place);
+
+$status = insertStagiaires($tabStag);
+
+if ($status) {
+    echo "insert ok!!!";
+} else {
+    echo "insert KO!!!";
+}
+
+?>
