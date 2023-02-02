b0e50b1d0d7cee7937ce9f969c07dbee266b8a90
59013-90-07 <59013-90-07@exchange.ad.afpanet>
2023-02-02 09:26:56 +0100

    --TP5

--- 7-TP5/controller/exeSearch.php
+++
@@ -1,7 +0,0 @@
-<!-- <?php
-// include '../modele/DBManagement.php';
-
-// $stagiare = serachStagByname("LAMRI");
-
-// var_dump($stagiare); -->
-// $sql = "SELECT (select concat (nom, prenom) as nomPrenom from stagiaire )as req WHERE nomPrenom like '%?' ;";
