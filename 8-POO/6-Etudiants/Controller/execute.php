b1ece24d99fd2b96093396c3ba2e3c9552074ea1
59013-90-03 <59013-90-03@exchange.ad.afpanet>
2023-02-13 16:07:04 +0100

    Modif 6-Etudiants

--- 8-POO/6-Etudiants/Controller/execute.php
+++ 8-POO/6-Etudiants/Controller/execute.php
@@ -5,32 +5,33 @@ include_once "../Modele/Filiere.class.php";
 
 echo "La liste des filières de notre école : \n";
 
-$filiere1 = new Filiere(00, 'Informatique');
-$filiere2 = new Filiere(01, "Techno de l'info et de la communication");
-$filiere3 = new Filiere(02, 'Génie civil');
+$filiere[1] = new Filiere(00, 'Informatique');
+$filiere[2] = new Filiere(01, "Techno de l'info et de la communication");
+$filiere[3] = new Filiere(02, 'Génie civil');
 
-echo  $filiere1;
-echo  $filiere2;
-echo  $filiere3;
-
-$etudiant1 = new Etudiants('Mohamed', 'Rashid', '1992/01/02', $filiere1);
-
-$etudiant2 = new Etudiants('Yves','Chakib', '1992/04/02', $filiere1);
-
-$etudiant3 = new Etudiants('Martin', 'Manal', '2011/03/02', $filiere2);
-
-$etudiant4 = new Etudiants('Jacques', 'Meriem', '2000/11/02', $filiere3);
-
-$etudiant5 = new Etudiants('Remi', 'Mouad', '2011/03/02', $filiere3);
+echo  $filiere[1];
+echo  $filiere[2];
+echo  $filiere[3];
 
 
 
-echo $etudiant1."\n";
+$etudiant[1] = new Etudiants('Mohamed', 'Rashid', '1992/01/02', $filiere[1]);
 
-echo $etudiant2."\n"; 
+$etudiant[2] = new Etudiants('Yves','Chakib', '1992/04/02', $filiere[1]);
 
-echo $etudiant3."\n";
+$etudiant[3] = new Etudiants('Martin', 'Manal', '2011/03/02', $filiere[2]);
 
-echo $etudiant4."\n";
+$etudiant[4] = new Etudiants('Jacques', 'Meriem', '2000/11/02', $filiere[3]);
 
-echo $etudiant5."\n";
+$etudiant[5] = new Etudiants('Remi', 'Mouad', '2011/03/02', $filiere[3]);
+
+echo $etudiant[1]."\n";
+
+echo $etudiant[2]."\n"; 
+
+echo $etudiant[3]."\n";
+
+echo $etudiant[4]."\n";
+
+echo $etudiant[5]."\n";
+
