137ecee57874630c76cfeb891f4422be4105d2b4
LucasV0 <lucas.vankes@hotmail.fr>
2023-02-15 21:16:11 +0100

    message

---
+++ 8-POO/8-Personne_Utilisateur/modele/Profil.class.php
@@ -0,0 +1,82 @@
+<?php
+
+class Profil
+{
+    private static $compteur;
+    private $id;
+    private $code;
+    private $libelle;
+
+    public function __construct($code, $libelle)
+    {
+        self::$compteur++;
+        $this->id = self::$compteur;
+        $this -> code = $code;
+        $this -> libelle = $libelle; 
+    }
+
+    public function toString()
+   {
+    return $this->code . " " . $this->libelle;
+
+   }
+    /**
+     * Get the value of code
+     */ 
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set the value of code
+     *
+     * @return  self
+     */ 
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of libelle
+     */ 
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+
+    /**
+     * Set the value of libelle
+     *
+     * @return  self
+     */ 
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+}
