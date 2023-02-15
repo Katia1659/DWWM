4804d806b332d055c2e14acff779a5d175286299
59013-90-16 <59013-90-16@exchange.ad.afpanet>
2023-02-15 14:57:25 +0100

    8POO et exo personne utilisateur

---
+++ 8-POO/8-Personne_Utilisateur/Modele/Personne.class.php
@@ -0,0 +1,177 @@
+<?php
+
+// Création de l'objet et initialisation des attributs
+Class Personne{ 
+    protected int $id ;
+    public static $compteur ;
+    protected string $nom ;
+    protected string $prenom ; 
+    protected string $mail ;
+    protected string $tel ; 
+    protected float $salaire ;
+
+    public function __construct($nom , $prenom , $mail , $tel , $salaire)
+    {   
+        self:: $compteur++; 
+        $this -> id = self :: $compteur ;
+        $this -> nom = $nom ;
+        $this -> prenom = $prenom ; 
+        $this -> mail = $mail ;
+        $this -> tel = $tel ;
+        $this -> salaire = $salaire ;
+    }
+
+
+    // Creation des méthodes d'affichage et du calcul d'augmentation du salaire
+    public function affiche () : void {
+        echo "Identité : ".$this -> nom. " " .$this -> prenom."\nCoordonnées : \n".$this -> mail. " / " .$this -> tel."\nSalaire : " . $this -> salaire;
+    }
+
+    public function calculerSalaire (){
+       return $this -> salaire ;
+    }
+
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of compteur
+     */ 
+    public function getCompteur()
+    {
+        return $this->compteur;
+    }
+
+    /**
+     * Set the value of compteur
+     *
+     * @return  self
+     */ 
+    public function setCompteur($compteur)
+    {
+        $this->compteur = $compteur;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nom
+     */ 
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set the value of nom
+     *
+     * @return  self
+     */ 
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of prenom
+     */ 
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set the value of prenom
+     *
+     * @return  self
+     */ 
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of mail
+     */ 
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * Set the value of mail
+     *
+     * @return  self
+     */ 
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of tel
+     */ 
+    public function getTel()
+    {
+        return $this->tel;
+    }
+
+    /**
+     * Set the value of tel
+     *
+     * @return  self
+     */ 
+    public function setTel($tel)
+    {
+        $this->tel = $tel;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of salaire
+     */ 
+    public function getSalaire()
+    {
+        return $this->salaire;
+    }
+
+    /**
+     * Set the value of salaire
+     *
+     * @return  self
+     */ 
+    public function setSalaire($salaire)
+    {
+        $this->salaire = $salaire;
+
+        return $this;
+    }
+
+}
+
+?>
